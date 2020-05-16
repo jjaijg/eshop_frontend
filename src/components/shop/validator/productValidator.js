0761f8b991eebb81af42a62e7aba2378d7bc38f9
jjaijg <jjaiganeshjg95@gmail.com>
2020-05-16 18:34:18 +0530

    corrected error message dispaly logic for price fields

--- src/components/shop/validator/productValidator.js
+++ src/components/shop/validator/productValidator.js
@@ -1,14 +1,17 @@
 export const inputNumberValidator = (rule, value) => {
   let min = rule.min;
   let max = rule.max;
-  const message = rule.message;
+  const message = "Price should  be numeric.";
   if (min !== null) min = Number(min);
   if (max !== null) max = Number(max);
   try {
     const object = Number(value);
-    if (min !== null && object < min) throw new Error(message);
-    if (max !== null && object > max) throw new Error(message);
+    if (!value) return Promise.resolve();
     if (isNaN(object)) throw new Error(message);
+    if (min !== null && object < min)
+      throw new Error(`Price should be greater than ₹ ${min}`);
+    if (max !== null && object > max)
+      throw new Error(`Price should be less than ₹ ${max}`);
     return Promise.resolve();
   } catch (err) {
     return Promise.reject(err);
