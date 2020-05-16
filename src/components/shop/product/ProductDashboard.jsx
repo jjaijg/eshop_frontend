bb1920ddbf456f480a6e0e9fb26c8d0fe08ea19f
jjaijg <jjaiganeshjg95@gmail.com>
2020-05-16 18:35:41 +0530

    updated product dashboard to adapt tab logic

--- src/components/shop/product/ProductDashboard.jsx
+++ src/components/shop/product/ProductDashboard.jsx
@@ -1,18 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { Tabs } from "antd";
+
 import { useDispatch } from "react-redux";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import ProductList from "./ProductList.Component";
+import ProductAdd1 from "./ProductAdd1.component";
 import { getAllProducts } from "../../../app/dispatchers/productDispatchers";
 
 const ProductDashboard = (props) => {
   const dispatch = useDispatch();
+
+  const { TabPane } = Tabs;
+
   useEffect(() => {
     dispatch(getAllProducts({ pagination: { current: 1, pageSize: 10 } }));
   }, [getAllProducts]);
+
   return (
-    <>
-      <ProductList />
-    </>
+    <Tabs defaultActiveKey="2" type="card">
+      <TabPane tab="Product List" key="1">
+        <ProductList />
+      </TabPane>
+      <TabPane
+        tab="Add Product"
+        key="2"
+        closable={true}
+        style={{ marginTop: "32px" }}
+      >
+        <ProductAdd1 />
+      </TabPane>
+    </Tabs>
   );
 };
 
