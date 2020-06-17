c9df02b58ea28708ba02b05e762161f3574149bc
jjaijg <jjaiganeshjg95@gmail.com>
2020-06-18 00:30:17 +0530

    for future use

---
+++ src/components/shop/bill/BillCustomerForm.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const BillCustomerForm = (props) => {
+  return <div></div>;
+};
+
+BillCustomerForm.propTypes = {};
+
+export default BillCustomerForm;
