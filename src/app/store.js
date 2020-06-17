679202dd662a3baa97ce3e011d15943f8ca9e36a
jjaijg <jjaiganeshjg95@gmail.com>
2020-06-18 00:27:14 +0530

    added billReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import productReducer from "./reducers/productRecuder";
 import metricRecuder from "./reducers/metricReducer";
+import billReducer from "./reducers/billReducer";
 import logger from "redux-logger";
 
 const reducers = {
   product: productReducer,
   metric: metricRecuder,
+  bill: billReducer,
 };
 export default configureStore({
   reducer: { ...reducers },
